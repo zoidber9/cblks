08e4c0bf2936064cbe5fc6aff089748abf35c5b9
Zoidberg <zoidberg@debian>
2020-05-21 23:03:11 +0300

    trd

--- randomName/main.c
+++ randomName/main.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 int max (int A[], int n)
 {
-
+    int i;
+    int x = A[0];
+    for(i=0;i<n;i++)
+    {
+        if(A[i] < x)
+        {
+            A[i] == x;
+        }
+    }
+    return x;
 }
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("%d", max(a, 10));
 }
